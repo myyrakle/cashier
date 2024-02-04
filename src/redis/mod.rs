95b4abb3023a5e86ac95ffcb5e8ce5ef53473ed3
myyrakle <sssang97@naver.com>
2024-02-05 00:06:50 +0900

    [#3] impl RedisCashier

--- src/redis/mod.rs
+++ src/redis/mod.rs
@@ -1,4 +1,15 @@
 #[derive(Debug, Clone)]
 pub struct RedisCashier {
-    client: redis::Client,
+    client: Option<redis::Client>,
+}
+
+impl RedisCashier {
+    pub fn new() -> Self {
+        RedisCashier { client: None }
+    }
+
+    pub fn connect(&mut self, url: &str) -> anyhow::Result<()> {
+        self.client = Some(redis::Client::open(url)?);
+        Ok(())
+    }
 }
