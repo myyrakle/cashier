0800052ebddb0180a0f2beafec08b24ec3947909
myyrakle <sssang97@naver.com>
2024-02-06 00:41:34 +0900

    [#5] add: dynamo crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use cashier::Cashier;
 
 fn main() {
-    let mut cashier = cashier::redis::RedisCashier::new();
-    cashier.connect("redis://127.0.0.1/").unwrap();
-
-    cashier.set("key", "value").unwrap();
-    assert_eq!(cashier.get("key").unwrap().unwrap(), "value");
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_dynamodb::Client::new(&config);
 }
