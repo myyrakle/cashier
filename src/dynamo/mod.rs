9e87d45962a119f42fce6d88b9cc54d591d048c6
myyrakle <sssang97@naver.com>
2024-02-06 02:05:42 +0900

    [#5] impl DynamoCashier::create_table

--- src/dynamo/mod.rs
+++ src/dynamo/mod.rs
@@ -1,3 +1,10 @@
+use aws_sdk_dynamodb::{
+    operation::create_table,
+    types::{
+        builders::KeySchemaElementBuilder, AttributeDefinition, BillingMode, KeySchemaElement,
+        KeyType,
+    },
+};
 use tokio::runtime::Handle;
 
 pub use super::Cashier;
@@ -49,7 +56,7 @@ impl DynamoCashier {
 
     /// Check if the table exists
     pub fn check_exists(&self) -> anyhow::Result<bool> {
-       tokio::task::block_in_place(|| {
+        tokio::task::block_in_place(|| {
             Handle::current().block_on(async {
                 let client = match &self.client {
                     Some(client) => client,
@@ -61,21 +68,60 @@ impl DynamoCashier {
                     None => return Err(anyhow::anyhow!("table_name is not set")),
                 };
 
-                let describe_output = client.describe_table().table_name(table_name).send().await?;
-    
+                let describe_output = client
+                    .describe_table()
+                    .table_name(table_name)
+                    .send()
+                    .await?;
+
                 Ok(describe_output.table.is_some())
             })
         })
     }
 
-    /// Create table if not exists
-    pub fn create_table_if_not_exists(&self) -> anyhow::Result<()> {
-        tokio::task::block_in_place(|| {
+    /// Create table
+    pub fn create_table(&self) -> anyhow::Result<()> {
+        let result = tokio::task::block_in_place(|| {
             Handle::current().block_on(async {
-                self.client.
-            });
+                let client = match &self.client {
+                    Some(client) => client,
+                    None => return Err(anyhow::anyhow!("client is not set")),
+                };
+
+                let table_name = match &self.table_name {
+                    Some(table_name) => table_name,
+                    None => return Err(anyhow::anyhow!("table_name is not set")),
+                };
+
+                let _ = client
+                    .create_table()
+                    .table_name(table_name)
+                    .key_schema(
+                        KeySchemaElementBuilder::default()
+                            .attribute_name("key".to_string())
+                            .key_type(KeyType::Hash)
+                            .build()
+                            .unwrap(),
+                    )
+                    .billing_mode(BillingMode::PayPerRequest)
+                    .send()
+                    .await?;
+
+                Ok(())
+            })
         });
 
-        Ok(())
+        result
+    }
+
+    /// Create table if not exists
+    pub fn create_table_if_not_exists(&self) -> anyhow::Result<()> {
+        let exists = self.check_exists()?;
+
+        if exists {
+            return Ok(());
+        }
+
+        self.create_table()
     }
 }
